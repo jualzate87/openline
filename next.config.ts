05c26d54ea1e2ab4a546a268d07bc8d3eb2c92b9
Juan Alzate <jalzate@macos-mhhmv91w61.corp.intuit.net>
2026-03-24 10:44:14 -0700

    Fix Next.js config for static export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/openline",
-  unoptimizedImages: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
