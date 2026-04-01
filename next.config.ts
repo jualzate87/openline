38b95597fb221164d8cd1e9111f9c5c79dab79e2
Juan Alzate <jalzate@intuit.com>
2026-04-01 15:55:15 -0700

    Fix basePath for Intuit GitHub Pages URL

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Openline",
+  basePath: "/pages/jalzate/Openline",
   images: {
     unoptimized: true,
   },
